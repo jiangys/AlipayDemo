992c53eaaec7116b040ae4fe5f6f1069871badbd
jys <jys509@126.com>
2016-01-15 10:19:14 +0800

    Initial Commit

---
+++ AlipayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlipayDemo
+//
+//  Created by Apple on 16/1/15.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
